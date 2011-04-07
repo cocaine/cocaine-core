4bb4654e346a1a360067723b53b0498990f29ce2
Unknown Entity <me@kobology.ru>
2011-04-07 21:24:29 +0400

    Script fix

--- valgrind.sh
+++ valgrind.sh
@@ -2,9 +2,4 @@
 
 scons || exit 1
 
-LD_LIBRARY_PATH=lib valgrind --tool=memcheck --leak-check=full --track-origins=yes --show-reachable=yes bin/yappi -l tcp://*:1710 -e tcp://*:1711 -p plugins &
-PID=$!
-
-tailf /var/log/syslog | grep yappi
-
-kill -SIGINT $PID
+LD_LIBRARY_PATH=build valgrind --tool=memcheck --leak-check=full --track-origins=yes --show-reachable=yes build/yappi -l tcp://*:1710 -e tcp://*:1711 -p build/plugins
