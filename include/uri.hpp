6642cf4a8bec6634e24885372996cefe1624ff2e
Unknown Entity <me@kobology.ru>
2011-04-22 14:40:50 +0400

    Cosmetic fixes

--- include/uri.hpp
+++ include/uri.hpp
@@ -11,96 +11,96 @@
 namespace yappi { namespace helpers {
 
 struct uri_t {
-uri_t(const std::string& source):
-    m_source(source)
-{
-    // URI parser initialization
-    UriParserStateA state;
+    uri_t(const std::string& source):
+        m_source(source)
+    {
+        // URI parser initialization
+        UriParserStateA state;
 
-    state.uri = &m_uri;
+        state.uri = &m_uri;
    
-    int result;
-    if((result = uriParseUriA(&state, m_source.c_str())) != URI_SUCCESS) {
-        throw std::runtime_error(strerror(result));
+        int result;
+        if((result = uriParseUriA(&state, m_source.c_str())) != URI_SUCCESS) {
+            throw std::runtime_error(strerror(result));
+        }
     }
-}
 
-~uri_t() {
-    uriFreeUriMembersA(&m_uri);
-}
+    ~uri_t() {
+        uriFreeUriMembersA(&m_uri);
+    }
 
-std::string source() {
-    return m_source;
-}
+    std::string source() {
+        return m_source;
+    }
 
-std::string scheme() {
-    return std::string(m_uri.scheme.first, m_uri.scheme.afterLast);
-}
+    std::string scheme() {
+        return std::string(m_uri.scheme.first, m_uri.scheme.afterLast);
+    }
 
-std::string userinfo() {
-    return std::string(m_uri.userInfo.first, m_uri.userInfo.afterLast);
-}
+    std::string userinfo() {
+        return std::string(m_uri.userInfo.first, m_uri.userInfo.afterLast);
+    }
 
-std::string host() {
-    return std::string(m_uri.hostText.first, m_uri.hostText.afterLast);
-}            
+    std::string host() {
+        return std::string(m_uri.hostText.first, m_uri.hostText.afterLast);
+    }            
 
-unsigned int port() {
-    unsigned int port = 0;
+    unsigned int port() {
+        unsigned int port = 0;
     
-    if(m_uri.portText.first && m_uri.portText.afterLast) {
-        std::istringstream extract(std::string(
-            m_uri.portText.first, m_uri.portText.afterLast));
-        extract >> port;
+        if(m_uri.portText.first && m_uri.portText.afterLast) {
+            std::istringstream extract(std::string(
+                m_uri.portText.first, m_uri.portText.afterLast));
+            extract >> port;
+        }
+
+        return port;
     }
 
-    return port;
-}
+    std::vector<std::string> path() {
+        std::vector<std::string> path;
+        UriPathSegmentA *segment = m_uri.pathHead;
 
-std::vector<std::string> path() {
-    std::vector<std::string> path;
-    UriPathSegmentA *segment = m_uri.pathHead;
+        while(segment) {
+            path.push_back(std::string(segment->text.first, segment->text.afterLast));
+            segment = segment->next;
+        }
 
-    while(segment) {
-        path.push_back(std::string(segment->text.first, segment->text.afterLast));
-        segment = segment->next;
+        return path;
     }
 
-    return path;
-}
+    std::map<std::string, std::string> query() {
+        std::map<std::string, std::string> query;
 
-std::map<std::string, std::string> query() {
-    std::map<std::string, std::string> query;
-
-    if(m_uri.query.first && m_uri.query.afterLast) {
-        std::string src(m_uri.query.first, m_uri.query.afterLast);
-        size_t start = 0, delimiter, end;
+        if(m_uri.query.first && m_uri.query.afterLast) {
+            std::string src(m_uri.query.first, m_uri.query.afterLast);
+            size_t start = 0, delimiter, end;
     
-        do {
-            delimiter = src.find_first_of('=', start);
-            end = src.find_first_of('&', start);
+            do {
+                delimiter = src.find_first_of('=', start);
+                end = src.find_first_of('&', start);
 
-            if(delimiter != src.npos) {
-                query.insert(std::make_pair(
-                    std::string(src, start, delimiter - start),
-                    std::string(src, delimiter + 1, end - delimiter - 1)
-                ));
-            }
+                if(delimiter != src.npos) {
+                    query.insert(std::make_pair(
+                        std::string(src, start, delimiter - start),
+                        std::string(src, delimiter + 1, end - delimiter - 1)
+                    ));
+                }
 
-            start = end + 1;
-        } while(end != src.npos);
+                start = end + 1;
+            } while(end != src.npos);
+        }
+
+        return query;
     }
 
-    return query;
-}
+    std::string fragment() {
+        return std::string(m_uri.fragment.first, m_uri.fragment.afterLast);
+    }
 
-std::string fragment() {
-    return std::string(m_uri.fragment.first, m_uri.fragment.afterLast);
-}
-
-private:
-    std::string m_source;
-    UriUriA m_uri;
+    private:
+        std::string m_source;
+        UriUriA m_uri;
 };
 
 }}
