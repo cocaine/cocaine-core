1705ca34f395f9c6caff82cd4777cec18369ccfb
Unknown Entity <me@kobology.ru>
2011-09-15 16:44:34 +0400

    Bumped the valgrind script

--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-scons || exit 1
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo . || exit 1
 
-LD_LIBRARY_PATH=build valgrind --tool=memcheck --leak-check=full --track-origins=yes --show-reachable=yes build/yappi tcp://*:5100 --export tcp://*:5101 --watermark 1000
+LD_LIBRARY_PATH=build valgrind --tool=memcheck --leak-check=full --show-reachable=yes \
+    ./cocained tcp://*:45100 --export tcp://*:45101 --watermark 1000 \
+    --storage-driver files --storage-location build/tmp \
+    --instance valgrind
