7ead28aa085c0c4b575814ad9aac3fa0aa23f847
Unknown Entity <me@kobology.ru>
2011-09-29 18:25:02 +0400

    New, even better version of tuple merging

--- include/cocaine/helpers/tuples.hpp
+++ include/cocaine/helpers/tuples.hpp
@@ -1,69 +1,51 @@
 #ifndef COCAINE_TUPLES_HPP
 #define COCAINE_TUPLES_HPP
 
-#include <boost/mpl/list.hpp>
-#include <boost/mpl/push_front.hpp>
-#include <boost/mpl/joint_view.hpp>
-#include <boost/mpl/fold.hpp>
-
 #include "cocaine/common.hpp"
 
 namespace cocaine { namespace helpers {
 
 using namespace boost;
+using namespace boost::tuples;
 
 namespace detail {
-    template<class TupleType, class List = mpl::list<>::type>
-    struct tuple_type_list;
-    
-    template<class List>
-    struct tuple_type_list<tuples::null_type, List> {
-        typedef List type;
+    template<class LT, class RT>
+    struct chain {
+        typedef typename add_reference<typename LT::head_type>::type head_type;
+        typedef chain<typename LT::tail_type, RT> chain_type;
+        typedef cons<head_type, typename chain_type::type> type;
+
+        static type apply(LT& left, RT& right) {
+            return type(left.get_head(), chain_type::apply(left.get_tail(), right));
+        }
     };
     
-    template<class Head, class Tail, class List>
-    struct tuple_type_list<tuples::cons<Head, Tail>, List> {
-        typedef typename tuple_type_list<
-            Tail,
-            typename mpl::push_front<List, Head>::type
-        >::type type;
+    template<class RT>
+    struct chain<null_type, RT> {
+        typedef typename add_reference<typename RT::head_type>::type head_type;
+        typedef chain<typename RT::tail_type, null_type> chain_type;
+        typedef cons<head_type, typename chain_type::type> type;
+    
+        static type apply(null_type left, RT& right) {
+            return type(right.get_head(), chain_type::apply(right.get_tail(), left));
+        }
+    };
+    
+    template<>
+    struct chain<null_type, null_type> {
+        typedef null_type type;
+        static null_type null;
+
+        static type apply(null_type, null_type) {
+            return null;
+        }
     };
 }
 
-template<class T1, class T2>
-struct joint_tuple {
-    typedef typename mpl::joint_view<
-        typename detail::tuple_type_list<typename T2::inherited>::type,
-        typename detail::tuple_type_list<typename T1::inherited>::type
-    >::type type_list;
-
-    typedef typename mpl::fold<
-        type_list,
-        tuples::null_type,
-        tuples::cons<mpl::_2, mpl::_1>
-    >::type type;
-};
-
-namespace detail {
-    template<size_t Position, class R>
-    static void link(R& result, const tuples::null_type&) { }
-
-    template<size_t Position, class R, class T>
-    static void link(R& result, const T& source) {
-        result.get<Position>() = source.get_head();
-        link<Position + 1>(result, source.get_tail());
-    }
-}
-
-template<class T1, class T2>
-static typename joint_tuple<T1, T2>::type
-join_tuples(const T1& left, const T2& right) {
-    typename joint_tuple<T1, T2>::type result;
-
-    detail::link<0>(result, left);
-    detail::link<tuples::length<T1>::value>(result, right);
-
-    return result;
+template<class LT, class RT>
+static typename detail::chain<LT, RT>::type
+joint_view(LT& lt, RT& rt) {
+    return detail::chain<LT, RT>::apply(lt, rt);
 }
 
 }}
