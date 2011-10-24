8838a527910658e3aab5b805ab237cfe567fbfbb
Kobolog <me@kobology.ru>
2011-10-25 03:20:36 +0400

    general: a working valgrind script, again

--- valgrind.sh
+++ valgrind.sh
@@ -2,7 +2,9 @@
 
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo . || exit 1
 
-LD_LIBRARY_PATH=build valgrind --tool=memcheck --leak-check=full --show-reachable=yes \
-    ./cocained tcp://*:45100 --export tcp://*:45101 --watermark 1000 \
-    --storage-driver files --storage-location build/tmp \
-    --instance valgrind
+make
+
+valgrind --tool=memcheck --leak-check=full --show-reachable=yes \
+    ./cocained tcp://*:10000 --instance valgrind
+
+
