ff7156f4eaea8f5128841aa0606b709f26e766f3
Kobolog <me@kobology.ru>
2011-12-07 18:33:36 +0300

    general: licensing

--- src/drivers/native_server.cpp
+++ src/drivers/native_server.cpp
@@ -1,3 +1,16 @@
+//
+// Copyright (C) 2011 Andrey Sibiryov <me@kobology.ru>
+//
+// Licensed under the BSD 2-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #include "cocaine/drivers/native_server.hpp"
 #include "cocaine/engine.hpp"
 
