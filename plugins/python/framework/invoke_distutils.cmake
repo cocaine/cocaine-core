7279b77d6fa1ad7e0a0d6b7a09422951153030dc
Kobolog <me@kobology.ru>
2011-12-15 02:14:26 +0300

    general: fix some warnings, update makefiles

--- plugins/python/framework/invoke_distutils.cmake
+++ plugins/python/framework/invoke_distutils.cmake
@@ -1,4 +1,4 @@
 EXECUTE_PROCESS(
-    COMMAND python setup.py install --prefix=${CMAKE_INSTALL_PREFIX}
+    COMMAND python setup.py install --prefix=${CMAKE_INSTALL_PREFIX} --install-layout=deb
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/plugins/python/framework
 )
--- plugins/python/src/python.cpp
+++ plugins/python/src/python.cpp
@@ -30,13 +30,14 @@ python_t::python_t(const std::string& args):
 
     helpers::uri_t uri(args);
     helpers::download_t app(helpers::download(uri));   
+    char path_object_name[] = "path";
 
     // Acquire the interpreter state
     thread_state_t state;
-
+    
     // NOTE: Prepend the current application cache location to the sys.path,
     // so that it could import different stuff from there
-    PyObject* paths = PySys_GetObject("path");
+    PyObject* paths = PySys_GetObject(path_object_name);
 
     if(PyList_Check(paths)) {
         // XXX: Does it steal the reference or not?
