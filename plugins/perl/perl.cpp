13693d17f77997bb36014b2c79c5b1e5c6414674
Rim Zaidullin <tinybit@yandex.ru>
2012-01-27 17:13:46 +0300

    added ability for the perl plugin to use external modules. added proper perl exception handling.

--- plugins/perl/perl.cpp
+++ plugins/perl/perl.cpp
@@ -18,6 +18,16 @@
 #include <EXTERN.h>               /* from the Perl distribution     */
 #include <perl.h>                 /* from the Perl distribution     */
 
+EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
+
+EXTERN_C void xs_init(pTHX) {
+	char* file = (char*)__FILE__;
+	dXSUB_SYS;
+
+	// DynaLoader is a special case
+	newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+}
+
 namespace cocaine { namespace plugin {
 
 class perl_t:
@@ -47,11 +57,10 @@ class perl_t:
             perl_free(my_perl);
         }
             
-        virtual void invoke(
-            callback_fn_t callback,
-            const std::string& method,
-            const void* request,
-            size_t size)
+        virtual void invoke(callback_fn_t callback,
+        					const std::string& method,
+        					const void* request,
+        					size_t size)
         {
             std::string input;
             
@@ -62,55 +71,55 @@ class perl_t:
             std::string result;
             const char* input_value_buff = NULL;
 
+            // init stack pointer
+            dSP;
+            ENTER;
+            SAVETMPS;
+
+            // remember stack pointer
+            PUSHMARK(SP);
+
+            int call_flags = G_EVAL | G_SCALAR | G_NOARGS;
+
+            // if there's an argument - push it on the stack
             if (!input.empty()) {
+            	call_flags = G_EVAL | G_SCALAR;
                 input_value_buff = input.c_str();
 
-                dSP;
-                ENTER;
-                SAVETMPS;
-				
-                PUSHMARK(SP);
                 XPUSHs(sv_2mortal(newSVpv(input_value_buff, 0)));
                 PUTBACK;
-                
-				int ret_vals_count = call_pv(method.c_str(), G_SCALAR);
-                SPAGAIN;
-
-                if (ret_vals_count > 0) {
-                    char* str_ptr = savepv(POPp);
-
-                    if (str_ptr) {
-                        result = std::string(str_ptr);
-                    }
-                }
-
-                FREETMPS;
-                LEAVE;
-            }
-            else {
-                PERL_SET_CONTEXT(my_perl);
-                dSP;
-                ENTER;
-                SAVETMPS;
-
-                PUSHMARK(SP);				
-                PUTBACK;
-
-                int ret_vals_count = call_pv(method.c_str(), G_SCALAR | G_NOARGS);
-                SPAGAIN;
-
-                if (ret_vals_count > 0) {
-                    char* str_ptr = savepv(POPp);
-
-                    if (str_ptr) {
-                        result = std::string(str_ptr);
-                    }
-                }
-
-                FREETMPS;
-                LEAVE;
             }
 
+            // call function
+            int ret_vals_count = call_pv(method.c_str(), call_flags);
+
+            // refresh stack pointer
+			SPAGAIN;
+
+			// get error (if any)
+			if (SvTRUE(ERRSV)) {
+				STRLEN n_a;
+
+				std::string error_msg = "perl eval error: ";
+				error_msg += SvPV(ERRSV, n_a);
+				throw unrecoverable_error_t(error_msg);
+			}
+
+			// pop returned value of the stack
+			if (ret_vals_count > 0) {
+				char* str_ptr = savepv(POPp);
+
+				if (str_ptr) {
+					result = std::string(str_ptr);
+				}
+			}
+
+			// clean-up
+			PUTBACK;
+			FREETMPS;
+			LEAVE;
+
+			// invoke callback with resulting data
             if (!result.empty()) {
                 callback(result.data(), result.size());
             }
@@ -119,10 +128,9 @@ class perl_t:
         void compile(const std::string& code)
         {
             const char* embedding[] = {"", "-e", "0"};
-            perl_parse(my_perl, NULL, 3, (char**)embedding, NULL);
+            perl_parse(my_perl, xs_init, 3, (char**)embedding, NULL);
             PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
             perl_run(my_perl);
-
             eval_pv(code.c_str(), TRUE);
         }
     
