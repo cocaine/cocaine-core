9b03979182b6f65095306b568ff2c051e1d46386
Rim Zaidullin <tinybit@yandex.ru>
2012-03-02 14:48:17 +0300

    added tests and updated perl plugin

--- plugins/perl/perl.cpp
+++ plugins/perl/perl.cpp
@@ -20,149 +20,149 @@
 #include "cocaine/interfaces/plugin.hpp"
 #include "cocaine/registry.hpp"
 
-EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
+EXTERN_C void boot_DynaLoader(pTHX_ CV* cv);
 
 EXTERN_C void xs_init(pTHX) {
-	char* file = (char*)__FILE__;
-	dXSUB_SYS;
+    char* file = (char*)__FILE__;
+    dXSUB_SYS;
 
-	// DynaLoader is a special case
-	newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+    // DynaLoader is a special case
+    newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
 }
 
-namespace cocaine { namespace engine {
+namespace cocaine {
+namespace engine {
 
-class perl_t:
-    public plugin_t,
-    public core::module_t<perl_t>
-{
-    public:
-        perl_t(context_t& ctx):
-            plugin_t(ctx, "perl")
-        { }
+class perl_t: public plugin_t, public core::module_t<perl_t> {
+public:
+    perl_t(context_t& ctx) : plugin_t(ctx, "perl") {
+        PERL_SYS_INIT3(NULL, NULL, NULL);
 
-        ~perl_t() 
-        {
-            perl_destruct(my_perl);
-            perl_free(my_perl);
+        my_perl = perl_alloc();
+        perl_construct(my_perl);
+    }
+
+    ~perl_t() {
+        perl_destruct(my_perl);
+        perl_free(my_perl);
+        PERL_SYS_TERM();
+    }
+
+    virtual void initialize(const app_t& app) {
+        Json::Value args(app.manifest["args"]);
+
+        if(!args.isObject()) {
+            throw unrecoverable_error_t("malformed manifest");
         }
 
-        virtual void initialize(const app_t& app)
-        {
-            Json::Value args(app.manifest["args"]);
+        boost::filesystem::path source(args["source"].asString());
 
-            if(!args.isObject()) {
-                throw unrecoverable_error_t("malformed manifest");
-            }
-
-            boost::filesystem::path source(args["source"].asString());
-
-            if(source.empty()) {
-                throw unrecoverable_error_t("no code location has been specified");
-            }
-
-            boost::filesystem::ifstream input(source);
-    
-            if(!input) {
-                throw unrecoverable_error_t("unable to open " + source.string());
-            }
-
-            std::stringstream stream;
-            stream << input.rdbuf();
-            
-            my_perl = perl_alloc();
-            perl_construct(my_perl);
-
-            compile(stream.str());
+        if(source.empty()) {
+            throw unrecoverable_error_t("no code location has been specified");
         }
-            
-        virtual void invoke(io_t& io, const std::string& method)
-        {
-            std::string input;
-            
-            if (io.request && io.request_size > 0) {
-               input = std::string((const char*)io.request, io.request_size);
-            }
 
-            std::string result;
-            const char* input_value_buff = NULL;
+        if(boost::filesystem::is_directory(source)) {
+            throw unrecoverable_error_t("malformed manifest, expected path to perl script, got a directory.");   
+        }
 
-            // init stack pointer
-            dSP;
-            ENTER;
-            SAVETMPS;
+        std::string source_dir;
+        #if BOOST_FILESYSTEM_VERSION == 3
+            source_dir = source.parent_path().string();
+        #else
+            source_dir = source.branch_path().string();
+        #endif
 
-            // remember stack pointer
-            PUSHMARK(SP);
+        boost::filesystem::ifstream input(source);
 
-            int call_flags = G_EVAL | G_SCALAR | G_NOARGS;
+        if(!input) {
+            throw unrecoverable_error_t("unable to open " + source.string());
+        }
 
-            // if there's an argument - push it on the stack
-            if (!input.empty()) {
-            	call_flags = G_EVAL | G_SCALAR;
-                input_value_buff = input.c_str();
+        const char* embedding[] = {"", (char*)source.string().c_str(), "-I", (char*)source_dir.c_str()};
+        perl_parse(my_perl, xs_init, 4, (char**)embedding, NULL);
+        PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+        log().info("%s", "running interpreter...");
+        perl_run(my_perl);
+    }
+        
+    virtual void invoke(io_t& io, const std::string& method) {
+        log().info("%s", (std::string("invoking method ") + method + "...").c_str());
+        std::string input;
+        
+        if (io.request && io.request_size > 0) {
+           input = std::string((const char*)io.request, io.request_size);
+        }
 
-                XPUSHs(sv_2mortal(newSVpv(input_value_buff, 0)));
-                PUTBACK;
-            }
+        PERL_SET_CONTEXT(my_perl);
 
-            // call function
-            int ret_vals_count = call_pv(method.c_str(), call_flags);
+        std::string result;
+        const char* input_value_buff = NULL;
 
-            // refresh stack pointer
-			SPAGAIN;
+        // init stack pointer
+        dSP;
+        ENTER;
+        SAVETMPS;
 
-			// get error (if any)
-			if (SvTRUE(ERRSV)) {
-				STRLEN n_a;
+        // remember stack pointer
+        PUSHMARK(SP);
 
-				std::string error_msg = "perl eval error: ";
-				error_msg += SvPV(ERRSV, n_a);
-				throw unrecoverable_error_t(error_msg);
+        int call_flags = G_EVAL | G_SCALAR | G_NOARGS;
+
+        // if there's an argument - push it on the stack
+        if (!input.empty()) {
+        	call_flags = G_EVAL | G_SCALAR;
+            input_value_buff = input.c_str();
+
+            XPUSHs(sv_2mortal(newSVpv(input_value_buff, 0)));
+            PUTBACK;
+        }
+
+        // call function
+        int ret_vals_count = call_pv(method.c_str(), call_flags);
+
+        // refresh stack pointer
+		SPAGAIN;
+
+		// get error (if any)
+		if (SvTRUE(ERRSV)) {
+			STRLEN n_a;
+
+			std::string error_msg = "perl eval error: ";
+			error_msg += SvPV(ERRSV, n_a);
+			throw unrecoverable_error_t(error_msg);
+		}
+
+		// pop returned value of the stack
+		if (ret_vals_count > 0) {
+			char* str_ptr = savepv(POPp);
+
+			if (str_ptr) {
+				result = std::string(str_ptr);
 			}
+		}
 
-			// pop returned value of the stack
-			if (ret_vals_count > 0) {
-				char* str_ptr = savepv(POPp);
+		// clean-up
+		PUTBACK;
+		FREETMPS;
+		LEAVE;
 
-				if (str_ptr) {
-					result = std::string(str_ptr);
-				}
-			}
-
-			// clean-up
-			PUTBACK;
-			FREETMPS;
-			LEAVE;
-
-			// invoke callback with resulting data
-            if (!result.empty()) {
-                io.push(result.data(), result.size());
-            }
+		// invoke callback with resulting data
+        if (!result.empty()) {
+            io.push(result.data(), result.size());
         }
+    }
 
-    private:
-        void compile(const std::string& code)
-        {
-            const char* embedding[] = {"", "-e", "0"};
-            perl_parse(my_perl, xs_init, 3, (char**)embedding, NULL);
-            PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
-            perl_run(my_perl);
-            eval_pv(code.c_str(), TRUE);
-        }
-    
-    private:
-        PerlInterpreter* my_perl;
+private:
+    PerlInterpreter* my_perl;
 };
 
 extern "C" {
     void initialize(core::registry_t& registry) {
-        PERL_SYS_INIT3(NULL, NULL, NULL);
+        
         registry.install("perl", &perl_t::create);
     }
 
     __attribute__((destructor)) void finalize() {
-        PERL_SYS_TERM();
     }
 }
 
---
+++ tests/dealer_test_app.cpp
@@ -0,0 +1,138 @@
+//
+// Copyright (C) 2011 Rim Zaidullin <creator@bash.org.ru>
+//
+// Licensed under the BSD 2-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <iostream>
+#include <sstream>
+
+#include <boost/program_options.hpp>
+#include <boost/bind.hpp> 
+#include <boost/filesystem.hpp> 
+
+#include <msgpack.hpp>
+
+#include "cocaine/dealer/client.hpp"
+#include "cocaine/dealer/details/time_value.hpp"
+
+namespace po = boost::program_options;
+namespace cd = cocaine::dealer;
+
+std::string config_path = "tests/config_example.json";
+
+static int count = 0;
+
+void response_callback(const cd::response& response, const cd::response_info& info) {
+	if (info.error != cd::MESSAGE_CHOKE) {
+
+		std::cout << "resp (CHUNK) uuid: " << response.uuid << std::endl;
+		//msgpack::unpacked msg;
+		//msgpack::unpack(&msg, (const char*)response.data, response.size);
+		//msgpack::object obj = msg.get();
+		//std::stringstream stream;
+
+		std::string st((const char*)response.data, response.size);
+
+		std::cout << "resp data: " << st << std::endl;
+	}
+	else {
+		//++count;
+		//if (count % 1000 == 0) {
+		//	std::cout << "count: " << count << std::endl;
+		//}
+
+		std::cout << "resp (CHOKE) uuid: " << response.uuid << std::endl;
+		std::cout << "resp done!" << std::endl;
+	}
+}
+
+void create_client(int add_messages_count) {
+	// create message path
+	cd::message_path path;
+	path.service_name = "perl-testing";
+	path.handle_name = "test_handle";
+
+	cd::client c(config_path);
+	c.connect();
+	c.set_response_callback(boost::bind(&response_callback, _1, _2), path);
+
+	sleep(3);
+
+	// send message
+	std::string message = "http://longcat.ru";
+	std::string uuid1 = c.send_message(message, path);
+	std::cout << "mesg uuid: " << uuid1 << std::endl;
+
+	/*
+	//lsd::time_value tv;
+	tv.init_from_current_time();
+
+	// create message policy
+	lsd::message_policy policy;
+	policy.deadline = tv.as_double() + 10.0;
+
+	// create message data
+	std::map<std::string, int> event;
+	event["service"] = 1;
+	event["uid"] = 12345;
+	event["score"] = 500;
+
+	msgpack::sbuffer buffer;
+	msgpack::pack(buffer, event);
+
+	std::string message = "whoa!";
+
+	// send messages
+	for (int i = 0; i < add_messages_count; ++i) {
+		std::string uuid1 = c.send_message(message, path);
+		std::cout << "mesg uuid: " << uuid1 << std::endl;
+	}
+	*/
+
+	sleep(5);
+}
+
+int
+main(int argc, char** argv) {
+	try {
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help", "Produce help message")
+			("client,c", "Start as server")
+			("messages,m", po::value<int>(), "Add messages to server")
+		;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			std::cout << desc << std::endl;
+			return EXIT_SUCCESS;
+		}
+
+		if (vm.count("client")) {
+			int add_messages_count = vm.count("messages") ? vm["messages"].as<int>() : 0;
+			create_client(add_messages_count);
+
+			return EXIT_SUCCESS;
+		}
+
+		std::cout << desc << std::endl;
+		return EXIT_FAILURE;
+	}
+	catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
