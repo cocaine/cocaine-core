79ee4b1521d761eebe51e5f3592d2410d523e057
Kobolog <me@kobology.ru>
2012-03-11 20:20:15 +0400

    forgot a new header

---
+++ include/cocaine/exceptions.hpp
@@ -0,0 +1,44 @@
+//
+// Copyright (C) 2011-2012 Andrey Sibiryov <me@kobology.ru>
+//
+// Licensed under the BSD 2-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef COCAINE_EXCEPTIONS_HPP
+#define COCAINE_EXCEPTIONS_HPP
+
+#include <stdexcept>
+
+namespace cocaine {
+
+// Application exceptions
+// ----------------------
+
+class unrecoverable_error_t:
+    public std::runtime_error
+{
+    public:
+        unrecoverable_error_t(const std::string& what):
+            std::runtime_error(what)
+        { }
+};
+
+class recoverable_error_t:
+    public std::runtime_error
+{
+    public:
+        recoverable_error_t(const std::string& what):
+            std::runtime_error(what)
+        { }
+};
+
+}
+
+#endif
