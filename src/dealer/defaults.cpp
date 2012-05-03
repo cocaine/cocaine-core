a8a8cdce07a56a3585362bf218f8bd7654ede7f0
Rimz <creator@bash.org.ru>
2012-05-03 21:09:12 +0400

    added missing files

---
+++ src/dealer/defaults.cpp
@@ -0,0 +1,23 @@
+//
+// Copyright (C) 2011-2012 Rim Zaidullin <creator@bash.org.ru>
+//
+// Licensed under the BSD 2-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "cocaine/dealer/defaults.hpp"
+
+namespace cocaine {
+namespace dealer {
+
+const std::string defaults::eblob_path = "/tmp/pmq_eblob";
+const std::string defaults::heartbeat_multicast_ip = "226.1.1.1";
+
+} // namespace dealer
+} // namespace cocaine
