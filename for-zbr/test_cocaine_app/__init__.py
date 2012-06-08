9cb92777b6caeff869868fd071bc261a48bfd7c2
Kobolog <me@kobology.ru>
2012-06-08 19:28:25 +0400

    bump

--- for-zbr/test_cocaine_app/__init__.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-
-def rimz_func(io):
-	#sleep(0.15)
-	input_data = io.read()
-	io.write(input_data + "1")
-	io.write(input_data + "2")
-	#raise RuntimeError("huita happened at rimz app!")
