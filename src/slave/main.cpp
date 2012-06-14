4785df774e72faf0d974bce0cd23ef0b32a2aab3
Kobolog <me@kobology.ru>
2012-06-14 16:56:56 +0400

    build fixes

--- src/slave/main.cpp
+++ src/slave/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char * argv[]) {
         ("help,h", "show this message")
         ("version,v", "show version and build information")
         ("configuration,c", po::value<std::string>
-            ()->default_value("/etc/cocaine/default.json"),
+            ()->default_value("/etc/cocaine/cocaine.conf"),
             "location of the configuration file")
         ("verbose", "produce a lot of output");
 
--- tools/deploy/deploy.cpp
+++ tools/deploy/deploy.cpp
@@ -141,7 +141,7 @@ int main(int argc, char * argv[]) {
         ("help,h", "show this message")
         ("version,v", "show version and build information")
         ("configuration,c", po::value<std::string>
-            ()->default_value("/etc/cocaine/default.json"),
+            ()->default_value("/etc/cocaine/cocaine.conf"),
             "location of the configuration file")
         ("manifest,m", po::value<std::string>
             ()->default_value("manifest.json"),
