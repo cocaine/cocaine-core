04d89ebd4ea0f93527bc499230b5df4a54f8a1ff
Kobolog <me@kobology.ru>
2012-09-26 03:02:02 +0400

    drop built-in jsoncpp (de-)serializer in favor of msgpack

--- include/cocaine/traits.hpp
+++ include/cocaine/traits.hpp
@@ -91,7 +91,59 @@ struct type_traits<Json::Value> {
     pack(msgpack::packer<Stream>& packer,
          const Json::Value& object)
     {
-        packer << helpers::json::serialize(object);
+        switch(object.type()) {
+            case Json::objectValue: {
+                packer.pack_map(object.size());
+
+                Json::Value::Members keys(object.getMemberNames());
+
+                for(Json::Value::Members::const_iterator it = keys.begin();
+                    it != keys.end();
+                    ++it)
+                {
+                    packer << *it;
+                    pack(packer, object[*it]);
+                }
+
+                break;
+            }
+
+            case Json::arrayValue:
+                packer.pack_array(object.size());
+
+                for(Json::Value::const_iterator it = object.begin();
+                    it != object.end();
+                    ++it)
+                {
+                    pack(packer, *it);
+                }
+
+                break;
+
+            case Json::booleanValue:
+                packer << object.asBool();
+                break;
+
+            case Json::stringValue:
+                packer << object.asString();
+                break;
+
+            case Json::realValue:
+                packer << object.asDouble();
+                break;
+
+            case Json::intValue:
+                packer << object.asLargestInt();
+                break;
+
+            case Json::uintValue:
+                packer << object.asLargestUInt();
+                break;
+
+            case Json::nullValue:
+                packer << msgpack::type::nil();
+                break;
+        }
     }
     
     static
@@ -99,18 +151,59 @@ struct type_traits<Json::Value> {
     unpack(const msgpack::object& packed,
            Json::Value& object)
     {
-        if(packed.type != msgpack::type::RAW) {
-            throw msgpack::type_error();
-        }
+        switch(packed.type) {
+            case msgpack::type::MAP: {
+                msgpack::object_kv * ptr = packed.via.map.ptr,
+                                   * const end = ptr + packed.via.map.size;
 
-        Json::Reader reader(Json::Features::strictMode());
-        
-        if(!reader.parse(
-            packed.via.raw.ptr,
-            packed.via.raw.ptr + packed.via.raw.size,
-            object))
-        {
-            throw msgpack::type_error();
+                for(; ptr < end; ++ptr) {
+                    if(ptr->key.type != msgpack::type::RAW) {
+                        // Key is not a string.
+                        throw msgpack::type_error();
+                    }
+
+                    unpack(
+                        ptr->val,
+                        object[ptr->key.as<std::string>()]
+                    );
+                }
+
+                break;
+            }
+
+            case msgpack::type::ARRAY: {
+                msgpack::object * ptr = packed.via.array.ptr,
+                                * const end = ptr + packed.via.array.size;
+                
+                for(unsigned int index = 0; ptr < end; ++ptr, ++index) {
+                    unpack(*ptr, object[index]);
+                }
+
+                break;
+            }
+
+            case msgpack::type::RAW:
+                object = packed.as<std::string>();
+                break;
+
+            case msgpack::type::DOUBLE:
+                object = packed.as<double>();
+                break;
+
+            case msgpack::type::POSITIVE_INTEGER:
+                object = static_cast<Json::UInt64>(packed.as<uint64_t>());
+                break;
+
+            case msgpack::type::NEGATIVE_INTEGER:
+                object = static_cast<Json::Int64>(packed.as<int64_t>());
+                break;
+
+            case msgpack::type::BOOLEAN:
+                object = packed.as<bool>();
+                break;
+
+            case msgpack::type::NIL:
+                break;
         }
     }
 };
