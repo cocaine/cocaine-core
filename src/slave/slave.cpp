62bb852da561e11388385cf04daace8e9745870c
Andrey Sibiryov <me@kobology.ru>
2012-10-11 21:35:16 +0400

    0.9.5 again; fixes for control

--- src/slave/slave.cpp
+++ src/slave/slave.cpp
@@ -88,22 +88,22 @@ void slave_t::configure() {
         io::command<rpc::error> command(server_error, e.what());
         m_bus.send(m_app, command);
         terminate();
-        throw();
+        throw;
     } catch(const repository_error_t& e) {
         io::command<rpc::error> command(server_error, e.what());
         m_bus.send(m_app, command);
         terminate();
-        throw();
+        throw;
     } catch(const unrecoverable_error_t& e) {
         io::command<rpc::error> command(server_error, e.what());
         m_bus.send(m_app, command);
         terminate();
-        throw();
+        throw;
     } catch(const std::exception& e) {
         io::command<rpc::error> command(server_error, e.what());
         m_bus.send(m_app, command);
         terminate();
-        throw();
+        throw;
     } catch(...) {
         io::command<rpc::error> command(
             server_error,
@@ -112,7 +112,7 @@ void slave_t::configure() {
         
         m_bus.send(m_app, command);
         terminate();
-        throw();
+        throw;
     }
 }
 
