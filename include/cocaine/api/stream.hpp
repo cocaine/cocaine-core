207e54c012005e4f9455b6c16b17f292bda121cd
Andrey Sibiryov <me@kobology.ru>
2012-11-13 19:20:13 +0400

    forgotten files

---
+++ include/cocaine/api/stream.hpp
@@ -0,0 +1,53 @@
+#ifndef COCAINE_STREAM_API_HPP
+#define COCAINE_STREAM_API_HPP
+
+#include "cocaine/common.hpp"
+#include "cocaine/traits.hpp"
+
+namespace cocaine { namespace api {
+
+struct stream_t {
+    virtual
+    ~stream_t() {
+        // Empty.
+    }
+    
+    virtual
+    void
+    push(const void * chunk,
+         size_t size) = 0;
+
+    template<class T>
+    void
+    push(const T& object) {
+        msgpack::sbuffer buffer;
+        msgpack::packer<msgpack::sbuffer> packer(buffer);
+
+        io::type_traits<T>::pack(buffer, object);
+
+        push(buffer.data(), buffer.size());
+    }
+
+    template<class T>
+    friend
+    stream_t&
+    operator << (stream_t& stream,
+                 const T& object)
+    {
+        stream.push(object);
+        return stream;
+    }
+
+    virtual
+    void
+    close() = 0;
+
+    virtual
+    void
+    abort(error_code code,
+          const std::string& message) = 0;
+};
+
+}}
+
+#endif
