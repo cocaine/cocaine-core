0e05c06570153f7c72f6bf76b2990803a109ea7b
Andrey Sibiryov <me@kobology.ru>
2013-03-20 18:19:19 +0400

    start all the configured services with runtime, drop the commandline options

--- src/runtime/bootstrap.cpp
+++ src/runtime/bootstrap.cpp
@@ -18,9 +18,8 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "cocaine/api/service.hpp"
-
 #include "cocaine/common.hpp"
+#include "cocaine/api/service.hpp"
 #include "cocaine/asio/service.hpp"
 #include "cocaine/config.hpp"
 #include "cocaine/context.hpp"
@@ -41,8 +40,7 @@ namespace po = boost::program_options;
 
 namespace {
     struct runtime_t {
-        runtime_t(context_t& context,
-                  const std::vector<std::string>& services):
+        runtime_t(context_t& context):
             m_context(context),
             m_log(new log_t(context, "runtime"))
         {
@@ -72,23 +70,24 @@ namespace {
             COCAINE_LOG_INFO(
                 m_log,
                 "initializing %d %s",
-                services.size(),
-                services.size() == 1 ? "service" : "services"
+                m_context.config.services.size(),
+                m_context.config.services.size() == 1 ? "service" : "services"
             );
 
-            for(std::vector<std::string>::const_iterator it = services.begin();
-                it != services.end();
-                ++it)
-            {
+            auto it = m_context.config.services.begin(),
+                 end = m_context.config.services.end();
+
+            for(; it != end; ++it) {
                 try {
-                    m_services.emplace_back(*it, api::service(m_context, *it));
+                    m_services.emplace_back(it->first, api::service(m_context, it->first));
                 } catch(const cocaine::error_t& e) {
                     throw cocaine::error_t(
                         "unable to initialize the '%s' service - %s",
-                        *it,
+                        it->first,
                         e.what()
                     );
                 }
+
             }
         }
 
@@ -145,10 +144,7 @@ namespace {
 }
 
 int main(int argc, char * argv[]) {
-    po::options_description general_options("General options"),
-                            service_options("Service options"),
-                            combined_options;
-
+    po::options_description general_options("General options");
     po::variables_map vm;
 
     general_options.add_options()
@@ -160,16 +156,10 @@ int main(int argc, char * argv[]) {
 #endif
         ("pidfile,p", po::value<std::string>(), "location of a pid file");
 
-    service_options.add_options()
-        ("service,s", po::value<std::vector<std::string>>(), "names of the services");
-
-    combined_options.add(general_options)
-                    .add(service_options);
-
     try {
         po::store(
             po::command_line_parser(argc, argv).
-                options(combined_options).
+                options(general_options).
                 run(),
             vm);
         po::notify(vm);
@@ -179,8 +169,8 @@ int main(int argc, char * argv[]) {
     }
 
     if(vm.count("help")) {
-        std::cout << cocaine::format("USAGE: %s <service-name> [options]", argv[0]) << std::endl;
-        std::cout << combined_options;
+        std::cout << cocaine::format("USAGE: %s [options]", argv[0]) << std::endl;
+        std::cout << general_options;
         return EXIT_SUCCESS;
     }
 
@@ -193,11 +183,6 @@ int main(int argc, char * argv[]) {
 
     if(!vm.count("configuration")) {
         std::cerr << "ERROR: no configuration file location has been specified." << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    if(!vm.count("service")) {
-        std::cerr << "ERROR: no services has been specified." << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -262,10 +247,7 @@ int main(int argc, char * argv[]) {
     std::unique_ptr<runtime_t> runtime;
 
     try {
-        runtime.reset(new runtime_t(
-            *context,
-            vm["service"].as<std::vector<std::string>>()
-        ));
+        runtime.reset(new runtime_t(*context));
     } catch(const cocaine::error_t& e) {
         std::cerr << cocaine::format(
             "ERROR: unable to initialize the runtime - %s.",
