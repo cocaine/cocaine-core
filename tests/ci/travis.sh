922c2979af90e8e6b3fac1a709c1ee1bba61c1fc
Andrey Sibiryov <me@kobology.ru>
2013-03-30 05:11:34 +0400

    zeromq must obviously die

--- tests/ci/travis.sh
+++ tests/ci/travis.sh
@@ -4,11 +4,18 @@
 yes | sudo add-apt-repository ppa:yandex-opensource/cocaine-dev
 sudo apt-get update -qq
 
-# Get rid of the broken Travis ZeroMQ package
+# Fix the initial package list
 yes | sudo apt-get remove libzmq3
+yes | sudo apt-get install wget
 
 # Install build-depends
 yes | sudo mk-build-deps -i
+
+# Link the broken ZeroMQ library to a right place
+ln -s /usr/lib/x86_64-linux-gnu/libzmq.so /usr/lib/libzmq.so
+
+# Fetch the now-gone ZeroMQ C++ bindings
+wget -O /usr/include/zmq.hpp https://raw.github.com/zeromq/cppzmq/master/zmq.hpp
 
 # Build packages
 yes | debuild -e CC -e CXX --prepend-path="/usr/local/bin/" -uc -us
