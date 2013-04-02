75c037af0966481a5c2475566c557145b6b4029b
Andrey Sibiryov <me@kobology.ru>
2013-04-03 03:25:37 +0400

    message type naming verbosity in dispatch object initializations

--- src/essentials/services/storage.cpp
+++ src/essentials/services/storage.cpp
@@ -35,8 +35,8 @@ storage_t::storage_t(context_t& context,
     category_type(context, reactor, name, args),
     m_storage(api::storage(context, "core"))
 {
-    on<storage::read  >("read",   std::bind(&api::storage_t::read,   m_storage, _1, _2));
-    on<storage::write >("write",  std::bind(&api::storage_t::write,  m_storage, _1, _2, _3));
-    on<storage::remove>("remove", std::bind(&api::storage_t::remove, m_storage, _1, _2));
-    on<storage::list  >("list",   std::bind(&api::storage_t::list,   m_storage, _1));
+    on<io::storage::read  >("read",   std::bind(&api::storage_t::read,   m_storage, _1, _2));
+    on<io::storage::write >("write",  std::bind(&api::storage_t::write,  m_storage, _1, _2, _3));
+    on<io::storage::remove>("remove", std::bind(&api::storage_t::remove, m_storage, _1, _2));
+    on<io::storage::list  >("list",   std::bind(&api::storage_t::list,   m_storage, _1));
 }
