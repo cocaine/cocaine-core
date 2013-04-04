ca1f83dc6cafe87423dae037faa9f0e93c7a3ff5
Andrey Sibiryov <me@kobology.ru>
2013-04-05 00:06:29 +0400

    no need to fetch zmq.hpp for travis-ci builds anymore, thanks to @redbaron

--- tests/ci/travis.sh
+++ tests/ci/travis.sh
@@ -6,16 +6,12 @@ sudo apt-get update -qq
 
 # Fix the initial package list
 yes | sudo apt-get remove libzmq3
-yes | sudo apt-get install wget
 
 # Install build-depends
 yes | sudo mk-build-deps -i
 
 # Link the broken ZeroMQ library to a right place
 sudo ln -s /usr/lib/x86_64-linux-gnu/libzmq.so /usr/lib/libzmq.so
-
-# Fetch the now-gone ZeroMQ C++ bindings
-sudo wget -O /usr/include/zmq.hpp https://raw.github.com/zeromq/cppzmq/master/zmq.hpp
 
 # Build packages
 yes | debuild -e CC -e CXX --prepend-path="/usr/local/bin/" -uc -us
