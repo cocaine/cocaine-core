c7def8dd902573293545fe9289ce79255b575224
Andrey Sibiryov <me@kobology.ru>
2013-05-23 19:56:23 +0400

    Unblock the signals correctly.
    Unblock the signals right before the execve().

--- src/isolates/process.cpp
+++ src/isolates/process.cpp
@@ -101,15 +101,7 @@ process_t::spawn(const std::string& path,
         ::dup2(pipe, STDOUT_FILENO);
         ::dup2(pipe, STDERR_FILENO);
 
-        // Unblock all the signals.
-
-        sigset_t signals;
-
-        sigfillset(&signals);
-
-        ::sigprocmask(SIG_UNBLOCK, &signals, nullptr);
-
-        // Prepare the arguments and environment.
+        // Prepare the arguments and environment
 
         size_t argc = args.size() * 2 + 2;
         // size_t envc = environment.size() + 1;
@@ -169,6 +161,16 @@ process_t::spawn(const std::string& path,
 
             std::_Exit(EXIT_FAILURE);
         }
+
+        // Unblock all the signals
+
+        sigset_t signals;
+
+        sigfillset(&signals);
+
+        ::sigprocmask(SIG_UNBLOCK, &signals, nullptr);
+
+        // Spawn the slave
 
         if(::execv(argv[0], argv) != 0) {
             std::error_code ec(errno, std::system_category());
