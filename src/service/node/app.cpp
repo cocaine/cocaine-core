6169ce6555ac30dc7af899776ed623a6a7e7274a
Evgeny Safronov <division494@gmail.com>
2015-03-20 13:28:13 +0300

    [Node] Make RPC IDL public.

--- src/service/node/app.cpp
+++ src/service/node/app.cpp
@@ -30,10 +30,10 @@
 #include "cocaine/detail/service/node/engine.hpp"
 #include "cocaine/detail/service/node/event.hpp"
 #include "cocaine/detail/service/node/manifest.hpp"
-#include "cocaine/detail/service/node/messages.hpp"
 #include "cocaine/detail/service/node/profile.hpp"
 #include "cocaine/detail/service/node/stream.hpp"
 
+#include "cocaine/idl/rpc.hpp"
 #include "cocaine/idl/streaming.hpp"
 
 #include "cocaine/logging.hpp"
