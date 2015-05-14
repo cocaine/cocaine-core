10519cecb2465c6786c787bf39ff901468526594
antmat <antmat@me.com>
2015-05-14 17:13:27 +0300

    [Zipkin] Code style.

--- include/cocaine/tracer/logger/blackhole.hpp
+++ include/cocaine/tracer/logger/blackhole.hpp
@@ -1,4 +1,25 @@
-#pragma once
+/*
+    Copyright (c) 2011-2015 Anton Matveenko <antmat@me.com>
+    Copyright (c) 2011-2015 Other contributors as noted in the AUTHORS file.
+
+    This file is part of Cocaine.
+
+    Cocaine is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Cocaine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser Gene ral Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef COCAINE_TRACER_LOGGER_BLACKHOLE
+#define COCAINE_TRACER_LOGGER_BLACKHOLE
 
 #include <cocaine/context.hpp>
 #include "cocaine/trace/logger.hpp"
@@ -47,3 +68,5 @@ private:
 };
 
 }}
+
+#endif
