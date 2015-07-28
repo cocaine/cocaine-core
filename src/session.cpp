19ae0f99e70de40a08dee540cbef034bd93a3f83
Andrey Sibiryov <kobolog@uber.com>
2015-07-28 18:05:40 -0400

    [I/O] Simplified remote_endpoint() implementation.

--- src/session.cpp
+++ src/session.cpp
@@ -374,43 +374,6 @@ session_t::remote_endpoint() const {
     return endpoint;
 }
 
-// TODO: Move to a separate TU.
-namespace {
-
-ip::tcp::endpoint
-to_tcp_endpoint(const generic::stream_protocol::endpoint& endpoint) {
-    switch (endpoint.protocol().family()) {
-    case AF_INET: {
-        const sockaddr_in* addr = reinterpret_cast<const sockaddr_in*>(endpoint.data());
-        ip::address_v4::bytes_type array;
-        std::copy((char*)&addr->sin_addr, (char*)&addr->sin_addr + array.size(), array.begin());
-
-        ip::address_v4 address(array);
-        return ip::tcp::endpoint(
-            address,
-            detail::socket_ops::network_to_host_short(addr->sin_port)
-        );
-    }
-    case AF_INET6: {
-        const sockaddr_in6* addrv6 = reinterpret_cast<const sockaddr_in6*>(endpoint.data());
-        ip::address_v6::bytes_type array;
-        std::copy((char*)&addrv6->sin6_addr, (char*)&addrv6->sin6_addr + array.size(), array.begin());
-
-        ip::address_v6 address(array, addrv6->sin6_scope_id);
-        return ip::tcp::endpoint(
-            address,
-            detail::socket_ops::network_to_host_short(addrv6->sin6_port)
-        );
-    }
-    default:
-        throw cocaine::error_t("invalid protocol");
-    };
-
-    return ip::tcp::endpoint();
-}
-
-} // namespace
-
 namespace cocaine {
 
 template<class Protocol>
@@ -423,7 +386,16 @@ session<Protocol>::session(std::unique_ptr<logging::log_t> log, std::unique_ptr<
 template<>
 typename session<ip::tcp>::endpoint_type
 session<ip::tcp>::remote_endpoint() const {
-    return ::to_tcp_endpoint(session_t::remote_endpoint());
+    const auto source = session_t::remote_endpoint();
+
+    BOOST_ASSERT(source.protocol() == ip::tcp::v4() || source.protocol() == ip::tcp::v6());
+
+    auto transformed = ip::tcp::endpoint();
+
+    transformed.resize(source.size());
+    std::memcpy(transformed.data(), source.data(), source.size());
+
+    return transformed;
 }
 
 template
