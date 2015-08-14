81d537cf2205cd4184862a183651459f9fcec784
Andrey Sibiryov <kobolog@uber.com>
2015-08-14 19:26:34 +0200

    [Runtime] Formatting.

--- src/runtime/runtime.cpp
+++ src/runtime/runtime.cpp
@@ -243,6 +243,7 @@ main(int argc, char* argv[]) {
 #endif
 
     // Logging
+
     const auto backend = vm["logging"].as<std::string>();
 
     std::cout << cocaine::format("[Runtime] Initializing the logging system, backend: %s.", backend)
@@ -252,16 +253,17 @@ main(int argc, char* argv[]) {
     std::unique_ptr<logging::log_t>    wrapper;
 
     try {
+        blackhole::attribute::set_t attributes;
+
         cocaine::logging::init_t logging(config->logging.loggers);
         logger = logging.logger(backend);
 
-        blackhole::attribute::set_t attributes;
-        if (logging.config(backend).attributes.count("source_host")) {
+        if(logging.config(backend).attributes.count("source_host")) {
             attributes.emplace_back("source_host", config->network.hostname);
         }
 
         wrapper.reset(new logging::log_t(*logger, attributes));
-    } catch(const std::out_of_range&) {
+    } catch(const std::out_of_range& e) {
         std::cerr << "ERROR: unable to initialize the logging - backend does not exist." << std::endl;
         return EXIT_FAILURE;
     }
