90bdcfe1f2ff61ac1f579cc5c86d6b9ec538ff92
antmat <antmat@me.com>
2015-08-19 21:26:37 +0300

    [Fix] Extend lifetime of encoded message in writable buffer.

--- include/cocaine/rpc/asio/writable_stream.hpp
+++ include/cocaine/rpc/asio/writable_stream.hpp
@@ -48,6 +48,7 @@ class writable_stream:
     typedef std::function<void(const std::error_code&)> handler_type;
 
     std::deque<asio::const_buffer> m_messages;
+    std::deque<typename Encoder::encoded_message_type> m_encoded_messages;
     std::deque<handler_type> m_handlers;
 
     enum class states { idle, flushing } m_state;
@@ -64,7 +65,7 @@ public:
     write(const message_type& message, handler_type handle) {
         size_t bytes_written = 0;
 
-        const auto encoded = encoder.encode(message);
+        auto encoded = encoder.encode(message);
 
         if(m_state == states::idle) {
             std::error_code ec;
@@ -83,6 +84,7 @@ public:
         );
 
         m_handlers.emplace_back(handle);
+        m_encoded_messages.emplace_back(std::move(encoded));
 
         if(m_state == states::flushing) {
             return;
@@ -115,6 +117,7 @@ private:
 
                 m_messages.pop_front();
                 m_handlers.pop_front();
+                m_encoded_messages.pop_front();
             }
 
             return;
@@ -137,6 +140,7 @@ private:
 
             m_messages.pop_front();
             m_handlers.pop_front();
+            m_encoded_messages.pop_front();
         }
 
         if(m_messages.empty() && m_state == states::flushing) {
