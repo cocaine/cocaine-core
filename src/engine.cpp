896afa0395fb7316a0719cc953ecc15c38688018
antmat <antmat@me.com>
2016-04-26 13:33:36 +0300

    fix(session): prevent race in session::pull

--- src/engine.cpp
+++ src/engine.cpp
@@ -187,11 +187,15 @@ execution_unit_t::attach(std::unique_ptr<Socket> ptr, const dispatch_ptr_t& disp
 
         // Create a new inactive session.
         session_ = std::make_shared<session_type>(std::move(log), std::move(transport), dispatch);
+        // Start pulling right now to prevent race when session is detached before pull
+        session_->pull();
     } catch(const std::system_error& e) {
         throw std::system_error(e.code(), "client has disappeared while creating session");
     }
 
-    m_asio->dispatch([=]() mutable { (m_sessions[fd] = std::move(session_))->pull(); });
+    m_asio->dispatch([=]() mutable {
+        m_sessions[fd] = std::move(session_);
+    });
 
     return session_;
 }
