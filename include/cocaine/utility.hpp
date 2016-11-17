25ab1dc19bf116619f7a83e9cd9ecd29fafce2fb
Evgeny Safronov <division494@gmail.com>
2016-11-17 19:45:09 +0300

    feat: more proper `result_of` implementation (#116)

--- include/cocaine/utility.hpp
+++ include/cocaine/utility.hpp
@@ -79,13 +79,21 @@ struct pristine {
 // Result-of type deduction
 
 template<class F, class = void>
-struct result_of {
-    typedef decltype(std::declval<F>()) type;
-};
+struct result_of : public result_of<decltype(&F::operator())> {};
 
 template<class F>
 struct result_of<F, typename depend<typename F::result_type>::type> {
     typedef typename F::result_type type;
+};
+
+template<class R, class... Args>
+struct result_of<R(*)(Args...), void> {
+    typedef R type;
+};
+
+template<class C, class R, class... Args>
+struct result_of<R(C::*)(Args...) const, void> {
+    typedef R type;
 };
 
 // Constant index sequences
