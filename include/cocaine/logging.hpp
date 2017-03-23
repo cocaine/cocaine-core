82b0b09a99d4fcfe80341635b98071c4dbd4e3b4
Evgeny Safronov <division494@gmail.com>
2017-03-23 20:58:31 +0300

    fix: restore used macro

--- include/cocaine/logging.hpp
+++ include/cocaine/logging.hpp
@@ -31,44 +31,50 @@
 
 // TODO: Do not include this file from public API.
 
+#define COCAINE_LOG(__log__, __severity__, ...) \
+    ::cocaine::detail::logging::log(__log__, __severity__, __VA_ARGS__)
+
 #define COCAINE_LOG_DEBUG(__log__, ...) \
-    cocaine::detail::logging::log(__log__, ::cocaine::logging::debug, __VA_ARGS__)
+    COCAINE_LOG(__log__, ::cocaine::logging::debug, __VA_ARGS__)
 
 #define COCAINE_LOG_INFO(__log__, ...) \
-    cocaine::detail::logging::log(__log__, ::cocaine::logging::info, __VA_ARGS__)
+    COCAINE_LOG(__log__, ::cocaine::logging::info, __VA_ARGS__)
 
 #define COCAINE_LOG_WARNING(__log__, ...) \
-    cocaine::detail::logging::log(__log__, ::cocaine::logging::warning, __VA_ARGS__)
+    COCAINE_LOG(__log__, ::cocaine::logging::warning, __VA_ARGS__)
 
 #define COCAINE_LOG_ERROR(__log__, ...) \
-    cocaine::detail::logging::log(__log__, ::cocaine::logging::error, __VA_ARGS__)
+    COCAINE_LOG(__log__, ::cocaine::logging::error, __VA_ARGS__)
 
 namespace cocaine { namespace detail { namespace logging {
 
-template<class T> inline auto logger_ref(T& log) -> T& { return log; }
-template<class T> inline auto logger_ref(T* const log) -> T& { return *log; }
-template<class T> inline auto logger_ref(std::unique_ptr<T>& log) -> T& { return *log; }
-template<class T> inline auto logger_ref(const std::unique_ptr<T>& log) -> T& { return *log; }
-template<class T> inline auto logger_ref(std::shared_ptr<T>& log) -> T& { return *log; }
-template<class T> inline auto logger_ref(const std::shared_ptr<T>& log) -> T& { return *log; }
+template<typename T> inline auto logger_ref(T& log) -> T& { return log; }
+template<typename T> inline auto logger_ref(T* const log) -> T& { return *log; }
+template<typename T> inline auto logger_ref(std::unique_ptr<T>& log) -> T& { return *log; }
+template<typename T> inline auto logger_ref(const std::unique_ptr<T>& log) -> T& { return *log; }
+template<typename T> inline auto logger_ref(std::shared_ptr<T>& log) -> T& { return *log; }
+template<typename T> inline auto logger_ref(const std::shared_ptr<T>& log) -> T& { return *log; }
 
-template<class T>
+template<typename T>
 auto
 make_facade(T&& log) -> blackhole::logger_facade<cocaine::logging::logger_t> {
     return blackhole::logger_facade<cocaine::logging::logger_t>(logger_ref(log));
 }
 
-template<class Log, class Message>
+template<typename Log, typename Message>
 auto
-log(Log&& log, cocaine::logging::priorities severity, Message&& message, const blackhole::attribute_list& l) -> void {
-    make_facade(log).log(severity, std::forward<Message>(message), l);
+log(Log&& log, cocaine::logging::priorities severity, Message&& message, const blackhole::attribute_list& attributes) -> void {
+    make_facade(log).log(severity, std::forward<Message>(message), attributes);
 }
 
-template<class Log, class Message, class... Args>
+template<typename Log, typename Message, typename... Args>
 auto
 log(Log&& log, cocaine::logging::priorities severity, Message&& message, const Args&... args) -> void {
-    make_facade(log).log(severity, std::forward<Message>(message),
-                         display_traits<typename pristine<Args>::type>::apply(args)...);
+    make_facade(log).log(
+        severity,
+        std::forward<Message>(message),
+        display_traits<typename pristine<Args>::type>::apply(args)...
+    );
 }
 
 }}}  // namespace cocaine::detail::logging
