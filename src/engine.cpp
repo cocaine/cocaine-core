8b3590eb549af5fb547719fef127bc66142adfd1
Evgeny Safronov <division494@gmail.com>
2017-05-16 17:07:48 +0300

    fix: check for nullptr

--- src/engine.cpp
+++ src/engine.cpp
@@ -191,7 +191,9 @@ execution_unit_t::attach(std::unique_ptr<Socket> ptr, const dispatch_ptr_t& disp
         // Start pulling right now to prevent race when session is detached before pull
         session_->pull();
 
-        dispatch->attached(session_);
+        if (dispatch) {
+            dispatch->attached(session_);
+        }
     } catch(const std::system_error& e) {
         throw std::system_error(e.code(), "client has disappeared while creating session");
     }
